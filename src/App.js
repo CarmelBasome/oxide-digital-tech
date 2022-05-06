28be86e6a9e770f571ad8b2e53f1b26ea0ee514b
CarmelBasome <carmelbaome@gmail.com>
2022-05-06 11:15:34 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -7,17 +7,26 @@ import Business from './components/Business';
 import Services from './components/Services';
 import Job from './components/Job';
 
-import {  AppBar } from '@mui/material';
+import {  AppBar, Typography, Card, CardMedia } from '@mui/material';
+import { Button } from '@mui/material';
 import Industries from './components/Industries'; 
 import Last_div from './components/Last_div';
 
 function App() {
   return <div>
-
     <AppBar>
       <Nav />
     </AppBar>
     <Idea />
+    <AppBar color='default' className="footer">
+      <div className='footerBackground'>
+        This website uses cookies to unsure you get the best experience on your website. 
+        <a href='#'>Learn more</a>
+        <div className='footer-float'>
+          <button className='footer-button'>Got it</button>
+        </div>
+      </div>
+    </AppBar>
     <Wwwus />
     <Business />
     <Services />
